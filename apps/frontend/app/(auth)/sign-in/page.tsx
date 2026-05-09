ab9fca4b9512bb0c3125725cced6bba1943b09bf
umernvd <muhammadumernaveed1234@gmail.com>
2026-05-09 19:25:21 +0500

    frontend: add sign-in page

---
+++ apps/frontend/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { SignInForm } from '@/components/auth/SignInForm'
+
+export const metadata = { title: 'Sign in' }
+
+export default function SignInPage(): React.JSX.Element {
+  return <SignInForm />
+}
