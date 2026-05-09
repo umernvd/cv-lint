102f432269e24d09a3388c5fcabbc616c5e87d56
umernvd <muhammadumernaveed1234@gmail.com>
2026-05-09 19:21:13 +0500

    backend: add auth module and barrel exports

---
+++ apps/backend/src/auth/auth.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+import { JwtStrategy } from './strategies'
+import { JwtAuthGuard } from './guards'
+import { UsersModule } from '../users'
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_ACCESS_EXPIRES_IN'),
+        },
+      }),
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService, JwtStrategy, JwtAuthGuard],
+  exports: [AuthService, JwtAuthGuard, JwtModule],
+})
+export class AuthModule {}
---
+++ apps/backend/src/auth/index.ts
@@ -0,0 +1,4 @@
+export { AuthModule } from './auth.module'
+export { AuthService } from './auth.service'
+export { AuthController } from './auth.controller'
+export { JwtAuthGuard } from './guards'
